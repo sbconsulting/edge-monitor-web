c0a82ddb2b938ab043cf8dec41cf36dd5ce7a469
Scott Baker <smbaker@gmail.com>
2021-12-01 11:08:29 -0800

    tabbed view

--- webui.js
+++ webui.js
@@ -45,9 +45,33 @@ function webui() {
         }
     }
 
+    onOpenTab = function(evt, tabName) {
+        // Declare all variables
+        var i, tabcontent, tablinks;
+      
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+      
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+      }
+
     initButtons = function() {
         $("#start").click(function() { webui.onStart(); });
         $("#stop").click(function() { webui.onStop(); })
+        $("#nav-ping").click(function(event) { webui.onOpenTab(event, "tab-ping"); })
+        $("#nav-traceroute").click(function(event) { webui.onOpenTab(event, "tab-traceroute"); })
+        $("#nav-dig").click(function(event) { webui.onOpenTab(event, "tab-dig"); })
     }
 
     startup = function() {
